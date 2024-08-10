a67f80c5f0cdfd67af7909649bdcf36f5530fa83
kyungminlee-12 <min2000.lee@gmail.com>
2024-08-10 22:29:12 +0900

    main oragnization

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from time import sleep
-from streamlit_option_menu import option_menu
 import pandas as pd
 import streamlit_shadcn_ui as ui
-from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
-
-import streamlit as st
-from streamlit_extras.stylable_container import stylable_container
 from streamlit_extras.colored_header import colored_header 
 from PIL import Image
+
+# from time import sleep
+# from streamlit_option_menu import option_menu
+# from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
+# from streamlit_extras.stylable_container import stylable_container
 
 
 im = Image.open("images/main_logo_green.png")
@@ -23,16 +22,6 @@ st.set_page_config(
     layout= "wide"
 )
 
-# 사이드바에 select box를 활용하여 종을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
-# st.sidebar.title('Location 설정')
-# select_location = st.sidebar.selectbox(
-#     '위치 데이터: ',
-#     ['화성','위치2', '위치3']
-# )
-
-# print("select_location:", select_location)
-
-# st.image('images\main_logo_green.png', caption='CNS INT')
 st.image(image_resize)  # caption='CNS INT'
 
 
@@ -72,9 +61,9 @@ def show_amt_c02_discharged_test():
     if min_res is None:
         min_res = 0
     
-    cur_all_res  = float(all_res)   # float(df_co2.loc[df_co2['id']=='ALL']["res_value"])
-    cur_hour_res = float(hour_res)   # float(df_co2.loc[df_co2['id']=='HOUR']["res_value"])
-    cur_min_res  = float(min_res)   # float(df_co2.loc[df_co2['id']=='MIN']["res_value"])
+    cur_all_res  = float(all_res)  
+    cur_hour_res = float(hour_res)   
+    cur_min_res  = float(min_res)   
 
     cols = st.columns(3)
     with cols[0]:
@@ -90,60 +79,24 @@ show_amt_c02_discharged_test()
 
 
 
-@st.experimental_fragment(run_every="2s")
-def show_amt_c02_discharged_per_min():
-    table_name="co2_discharged_min"  
-    select_data_query = """SELECT removed_amount, updated_date_time 
-                            FROM device_data.{}  
-                            order by updated_date_time desc
-                            LIMIT 20
-                            ;""".format(table_name)
-    datas_per_min = conn.query(select_data_query, ttl=3)
-    df_per_min = pd.DataFrame(datas_per_min)
-    st.dataframe(datas_per_min)
-    st.line_chart(df_per_min, y=['removed_amount'])
+# @st.experimental_fragment(run_every="2s")
+# def show_amt_c02_discharged_per_min():
+#     table_name="co2_discharged_min"  
+#     select_data_query = """SELECT removed_amount, updated_date_time 
+#                             FROM device_data.{}  
+#                             order by updated_date_time desc
+#                             LIMIT 20
+#                             ;""".format(table_name)
+#     datas_per_min = conn.query(select_data_query, ttl=3)
+#     df_per_min = pd.DataFrame(datas_per_min)
+#     st.dataframe(datas_per_min)
+#     st.line_chart(df_per_min, y=['removed_amount'])
 
 # show_amt_c02_discharged_per_min()
 
 
-cols = st.columns((1, 1, 2))
+# cols = st.columns((1, 1, 2))
 
-cols[0].metric("Temperature", "70 °F", "1.2 °F")
-cols[0].metric("Humidity", "86%", "4%")
-cols[0].metric("Wind", "9 mph", "-8%")
-
-# from streamlit_extras.altex import scatter_chart
-
-# https://arnaudmiribel.github.io/streamlit-extras/extras/altex/
-# @st.cache_data
-# def example_line():
-#     stocks = get_stocks_data()
-
-#     scatter_chart(
-#         data=stocks.query("symbol == 'GOOG'"),
-#         x="date",
-#         y="price",
-#         title="A beautiful simple line chart",
-#     )
-
-
-
-
-# with cols:
-# This column holds the data queried from the parts table.
-# st.header("CO2 감소량 list:")
-
-
-# st.write(df_co2["res_value"])
-# st.write(df_co2.loc[df_co2['id']=='MIN'])
-# st.write(df_co2.loc[df_co2['id']=='MIN']["res_value"])
-# st.write(df_co2.iloc[3]["res_value"])
-
-
-# 라인 그래프 데이터 생성 with Pandas
-# chart_data = pd.DataFrame(
-# 
-#     columns= ["IN", "OUT"]
-# )
-
-
+# cols[0].metric("Temperature", "70 °F", "1.2 °F")
+# cols[0].metric("Humidity", "86%", "4%")
+# cols[0].metric("Wind", "9 mph", "-8%")
